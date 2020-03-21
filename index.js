1eb808c702b1ce83dc264a36fa0d637e98252358
Chayapatr Archiwaranguprok <mail@chayapatr.com>
2020-03-21 16:22:12 +0700

    add figures generator function

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { createCard } = require("./src/createCard")
 const fs = require("fs")
 
-const data = require("./data")
+const data = require("./data.json")
 
 if (!fs.existsSync(`${__dirname}/dist/`))
     fs.mkdirSync(`${__dirname}/dist/`)
 
 data.forEach( data =>
-    createCard(data, `${__dirname}/dist/${data.keys.interest}.pdf`)
+    createCard(data, `${__dirname}/dist/${data.keys.interest}.pdf`, __dirname)
 )
--- src/createCard.js
+++ src/createCard.js
@@ -1,20 +1,25 @@
-const fs = require("fs");
-const PDFDocument = require("pdfkit");
+const fs = require("fs")
+const PDFDocument = require("pdfkit")
+const imageSize = require("image-size")
 
 const head = "fonts/avenirnext-demibold.ttf"
 const key = "fonts/avenirnext-regular.ttf"
 const body = "fonts/avenirnext-regular.ttf"
 
-function createCard(data, path) {
-  let doc = new PDFDocument({ size: "A4", margin: 50 });
+function createCard(data, path, dirname) {
+  let doc = new PDFDocument({
+    size: "A4",
+    margin: 50
+  })
 
-  generateHeader(doc);
-  generateKeys(doc, data);
-  generateBody(doc, data);
-  generateLinks(doc, data);
+  generateHeader(doc)
+  generateKeys(doc, data)
+  generateBody(doc, data)
+  generateLinks(doc, data)
+  generateFigures(doc, data, dirname)
 
-  doc.end();
-  doc.pipe(fs.createWriteStream(path));
+  doc.end()
+  doc.pipe(fs.createWriteStream(path))
 }
 
 function generateHeader(doc) {
@@ -22,7 +27,9 @@ function generateHeader(doc) {
     .font(head)
     .fontSize(36)
     .text("Scorecard", 50, 50)
-    .image('images/logo.png', 380, 15, {width: 200})
+    .image('images/logo.png', 380, 15, {
+      width: 200
+    })
 }
 
 function generateKeys(doc, data) {
@@ -30,26 +37,34 @@ function generateKeys(doc, data) {
     .fontSize(14)
     .font(head)
     .text(`Key of Interest:  ${data.keys.interest}`, 50, 115)
-    .text("Keywords: ", 50, doc.y + 5, { lineBreak: false })
+    .text("Keywords: ", 50, doc.y + 5, {
+      lineBreak: false
+    })
     .font(key)
     .text(data.keys.keywords, 130, doc.y)
     .font(head)
 
   doc
     .font(head)
-    .text("Impact: ", 50, doc.y + 5, { lineBreak: false })
+    .text("Impact: ", 50, doc.y + 5, {
+      lineBreak: false
+    })
     .font(key)
     .text(data.score.impact, 130, doc.y)
     .font(head)
-    .text("Certainity: ", 50, doc.y, { lineBreak: false })
+    .text("Certainity: ", 50, doc.y, {
+      lineBreak: false
+    })
     .font(key)
     .text(data.score.certainity, 130, doc.y)
     .font(head)
-    .text("Time Scale: ", 50, doc.y, { lineBreak: false })
+    .text("Time Scale: ", 50, doc.y, {
+      lineBreak: false
+    })
     .font(key)
     .text(data.score.timescale, 130, doc.y)
 
-  generateHr(doc, doc.y + 10);
+  generateHr(doc, doc.y + 10)
 }
 
 function generateBody(doc, data) {
@@ -62,20 +77,49 @@ function generateBody(doc, data) {
     .text("Implications: ", 50, doc.y + 10)
     .font(body)
     .text(data.body.implications, doc.x, doc.y + 5)
-    
-  generateHr(doc, doc.y + 10);
+
+  generateHr(doc, doc.y + 10)
 }
 
 function generateLinks(doc, data) {
   doc
-  .font("fonts/avenirnext-demibold.ttf")
-  .text("References/ Related Resources: ", 50, doc.y + 20)
-  .font(body)
+    .font(head)
+    .text("References/ Related Resources: ", 50, doc.y + 20)
+    .font(body)
+    .fillColor("#2C528C")
 
-  data.links.forEach( el => doc.text( el.name, {
+  data.links.forEach(el => doc.text(el.name, {
     link: el.link,
     underline: true
-  }) )
+  }))
+  generateHr(doc, doc.y + 10)
+}
+
+function generateFigures(doc, data, dirname) {
+  if (data.figures) {
+    doc
+      .fillColor("#000000")
+      .font(head)
+      .text("Figures: ", 50, doc.y + 20)
+      .font(body)
+    let count = 1
+    let prevHeight = 10
+    let addPage = 0
+    data.figures.forEach(el => {
+      if (doc.y >= 600) {
+        doc.addPage()
+        addPage = 1
+      }
+      const image = `${dirname}/images/${el.name}`
+      const dimensions = imageSize(image)
+      doc.image(image, 50, addPage ? 50 : doc.y + prevHeight, {
+          width: 200
+        }).text(`Figure ${count}: ${el.caption}`, 270, addPage ? 50 : doc.y + prevHeight)
+        ++count
+      prevHeight = (200 / dimensions.width) * dimensions.height
+      addPage = 0
+    })
+  }
 }
 
 function generateHr(doc, y) {
@@ -84,9 +128,9 @@ function generateHr(doc, y) {
     .lineWidth(1)
     .moveTo(50, y)
     .lineTo(550, y)
-    .stroke();
+    .stroke()
 }
 
 module.exports = {
   createCard
-};
+}
